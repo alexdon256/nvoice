42e7f5d35aeae358f969ddcd1c48f401ab4591e2
alexdon256 <oleksandr.don.256@gmail.com>
2025-04-20 05:27:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,35 +19,24 @@ setup(
             'nvoice=nvoice.main:main',
         ],
     },
-    dependency_links=[
-        'https://download.pytorch.org/whl/cu121'  # Specify the index URL
-    ],
     extras_require={
         'proto': ['protobuf==3.20.0'],  # Specific version for tts
     },
     install_requires=[
-        'pandas'
-       ,'xgboost'
-       ,'scikit-learn'
-       ,'numpy==1.22.0'
+        'numpy==1.22.0'
+       ,'num2words'
        ,'openai-whisper==20231117'
-       ,'torch==2.1.1'
-       ,'torchaudio==2.1.1'
-       ,'torchvision==0.16.1'
        ,'spleeter==2.4.2'
-       ,'soundstretch'
+       ,'audiostretchy'
        ,'httpx[http2]==0.19.0'
-       ,'pytube==15.0.0'
        ,'pydub==0.25.1'
-       ,'cffi==1.16.0'
-       ,'accelerate==0.26.1'
        ,'pyannote.audio==3.1.1'
-       ,'transformers==4.36.2'
-       ,'numba==0.58.1'
-       ,'deep-translator==1.11.4'
+       ,'transformers'
+       ,'numba'
+       ,'deep-translator'
        ,'ipython==7.34.0'
-       ,'ffmpeg-python==0.2.0'
-       ,'language-tool-python==2.7.1'
+       ,'ffmpeg-python'
+       ,'language-tool-python'
        ,'ffmpeg-python'
        ,'yt_dlp'
     ],
