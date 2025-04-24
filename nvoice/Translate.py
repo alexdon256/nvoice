f1b426d5bd0be17f2896e32adc57b86cb2ff57e8
alexdon256 <oleksandr.don.256@gmail.com>
2025-04-24 15:43:43 +0300

    Update Translate.py

--- nvoice/Translate.py
+++ nvoice/Translate.py
@@ -28,10 +28,9 @@ for rec in grammar_modifier.keys():
     seg = Segmenter()
     segments = seg(grammar_modifier[rec]) # Replace "audio.wav" with your audio file
     for segment in segments:
+        print(segment)
         if segment[0] == 'speech':
             grammar_modifier[rec[2]] = segment[2]
-            print('         processing '+rec[2])
-            print(segment[2])
             break
 
         
