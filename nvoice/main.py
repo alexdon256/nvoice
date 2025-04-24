76137b473b52c8bc3815ead0871efe1819082fe3
alexdon256 <oleksandr.don.256@gmail.com>
2025-04-24 14:16:22 +0300

    Update main.py

--- nvoice/main.py
+++ nvoice/main.py
@@ -30,7 +30,7 @@ def main():
         subprocess.run(['python',script_directory+f"/Diarize.py",proj, arg])
         subprocess.run(['python',script_directory+f"/Transcribe.py", proj, arg, sys.argv[3]])
         subprocess.run(['python',script_directory+f"/Translate.py", vocals, script_directory, sys.argv[3], sys.argv[4]])        
-        #subprocess.run(['python',script_directory+f"/synthesize.py", vocals, sys.argv[5], arg2])
+        #subprocess.run(['python',script_directory+f"/synthesize.py", proj, sys.argv[5], arg2])
         #subprocess.run(['python',script_directory+f"/RecoverVideo.py", video_path, vocals, sys.argv[1]])
         #shutil.rmtree(vocals)
         
