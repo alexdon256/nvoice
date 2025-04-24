3bac18cc642968c9b4faede7f4811663e009d0bd
alexdon256 <oleksandr.don.256@gmail.com>
2025-04-24 16:07:37 +0300

    Update Translate.py

--- nvoice/Translate.py
+++ nvoice/Translate.py
@@ -29,9 +29,9 @@ for rec in grammar_modifier.keys():
     segments = seg(grammar_modifier[rec]) # Replace "audio.wav" with your audio file
     for segment in segments:
         print('SEGMENT:     '+segment[0])
-        if segment[0] == 'speech':
-            grammar_modifier[rec[2]] = segment[1]
-            print('VALUE:     '+segment[1])
+        if segment[0] == 'male' or segment[0] == 'female':
+            grammar_modifier[rec[2]] = segment[0]
+            print('VALUE:     '+segment[0])
             break
 
         
